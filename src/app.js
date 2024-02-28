920538fe5c68bf5917727178172d9a9f39289878
Sudip Dhua <154453399+Sudip03Dhua@users.noreply.github.com>
2024-02-28 07:49:44 +0000

    added basic  express js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+//connecting to express libary
+const express = require('express');
+const path =require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/',(req,res) => {
+            // res.send('Hello Kq');
+            res.sendFile(path.join(__dirname, 'pages/index.html'));
+});
+
+const port =process.env.PORT || 3001;
+
+app.listen(port,()=>{
+    console.log(`Server running on https://localhost:${port}`);
+});
